f3bf9044d81c9797f219725e59d7f169a999a51f
Conte-Developer <samueledone08@gmail.com>
2026-01-02 18:30:01 +0100

    First syntax exercises

--- basics/tre_numeri.cs
+++ basics/tre_numeri.cs
@@ -1,1 +1,18 @@
-
+using System;
+class tre_numeri
+{
+    public static void Run()
+    {
+        int n = 0, mag = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("inserisci un numero intero:");
+            n = int.Parse(Console.ReadLine());
+            if (n>mag)
+            {
+                mag = n;
+            }
+        }
+        Console.WriteLine($"Il maggiore tra i numeri inseriti è {mag}");
+    }
+}
